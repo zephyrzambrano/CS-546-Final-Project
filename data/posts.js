9df0b72e265e3be3dba1de38d12be5384f352d69
Jugram-Chen <57543961+Jugram-Chen@users.noreply.github.com>
2020-04-12 15:00:06 -0400

    Update posts.js

--- data/posts.js
+++ data/posts.js
@@ -2,6 +2,20 @@ const mongoCollections = require('../config/mongoCollections');
 const posts = mongoCollections.posts;
 const { ObjectId } = require('mongodb');
 
+/*
+There are 11 properties in the post collection
+1.PostId:ObjectID
+2.topic:string
+3.userId:string
+4.content:string
+5.photoArr:arr[string of photo adress]
+6.commentIdArr:arr[string of commentId]
+7.tagArr:array[string of tags]
+8.likeCount[string of userId]
+9.dislikeCount[string of userId]
+10.viewCount:number
+11.date:Date object
+*/
 
 async function createPost(topic, userId, content, photoArr, tagArr) {//This function needs to interact with the user collection, and when a post is created, the user's postID needs to add a piece of data
     if (!topic || typeof topic !== "string")//此函数需要互动user collection，当创建了一个post，user的postID需要添加一条数据
@@ -21,8 +35,8 @@ async function createPost(topic, userId, content, photoArr, tagArr) {//This func
         userId: userId,
         content: content,
         photoArr: photoArr,
-        tagArr: tagArr,
         commentIdArr: [],
+        tagArr: tagArr,
         likeCount: [],
         dislikeCount: [],
         viewCount: 0,
@@ -30,9 +44,12 @@ async function createPost(topic, userId, content, photoArr, tagArr) {//This func
     }
     let insertInfo = await postCollection.insertOne(newPost);
     if (insertInfo.insertedCount === 0)
-        throw 'Could not add the Post';
+        throw 'Could not create the Post';
     let newId = insertInfo.insertedId;
     let postCreated = await this.getPostById(newId.toHexString());
+
+    usersCollection.addPostToUser(userId,newId.toHexString());//call the method in the user collection
+
     return postCreated;
 }
 
@@ -137,10 +154,14 @@ async function removePost(postId) {
     if (!postId || typeof postId !== "string") throw 'You must provide a post id';
     let postObjId = ObjectId.createFromHexString(postId);
     let postCollection = await posts();
+    let postInfo=await this.getPostById(postId);
     let deletionInfo = await postCollection.removeOne({ _id: postObjId });
     if (deletionInfo.deletedCount === 0) {
         throw `Could not delete the band with id of ${id}`;
     }
+
+    usersCollection.removePostId(postInfo.userId,postId);//call the method in the user collection
+
     return true;
 }
 
@@ -153,9 +174,10 @@ async function addCommentIdToPost(postId, commentId) {//此函数需要配合com
     postGoal.commentIdArr.push(commentId);
     let updatedInfo = await postCollection.updateOne({ _id: postObjId }, { commentIdArr: postGoal.commentIdArr });
     console.log(updatedInfo);
+    return true;
 }
 
-async function removeCommentIdToPost(postId, commentId) {//此函数需要配合comment collection使用，当一条comment数据被删除，应调用此函数
+async function removeCommentIdFromPost(postId, commentId) {//此函数需要配合comment collection使用，当一条comment数据被删除，应调用此函数
     if (!postId || typeof postId !== "string") throw 'You must provide a post id';
     if (!commentId || typeof commentId !== "string") throw 'You must provide a comment id';
     let postObjId = ObjectId.createFromHexString(postId);
@@ -169,6 +191,7 @@ async function removeCommentIdToPost(postId, commentId) {//此函数需要配合
     postGoal.commentIdArr=temp;
     let updatedInfo = await postCollection.updateOne({ _id: postObjId }, { commentIdArr: postGoal.commentIdArr });
     console.log(updatedInfo);
+    return true;
 }
 
 module.exports = {
@@ -183,7 +206,7 @@ module.exports = {
     addViewCount,
     removePost,
     addCommentIdToPost,
-    removeCommentIdToPost
+    removeCommentIdFromPost
 }
 
 

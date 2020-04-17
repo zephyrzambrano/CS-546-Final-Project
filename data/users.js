15409a051360b3c9c37814a19db13805db61de00
chuanyao17 <chuanyao17@gmail.com>
2020-04-17 18:46:42 -0400

    revised data/user.js, routes/data.js

--- data/users.js
+++ data/users.js
@@ -3,12 +3,10 @@ const users = mongoCollections.users;
 ObjectId = require('mongodb').ObjectID;
 
 
-async function createUser(username, password, nickname) {
-
+async function createUser(username, password, nickname) { //tested
     if (!username || typeof username !== "string") throw "must provide username";
     if (!password || typeof username !== "string") throw "must provide password";
     if (!nickname || typeof username !== "string") throw "must provide nickname";
-
     const userCollection = await users();
 
     let newUser = {
@@ -19,40 +17,47 @@ async function createUser(username, password, nickname) {
         posts: [], // array of postIds as strings
         comments: [] // array of commentIds as strings
     };
-
     const newInsertInformation = await userCollection.insertOne(newUser);
-	if (newInsertInformation.insertedCount === 0) throw 'Insert failed!';
+    if (newInsertInformation.insertedCount === 0) throw 'Insert failed!';
+    
 	return await this.getUserById(newInsertInformation.insertedId);
 };
 
-async function getUserById(userId) {
+async function getUserById(userId) { //updated string id to objid, tested
     const userCollection = await users();
+    if(typeof userId =="string")
+    {
+        const objId = ObjectId.createFromHexString(userId);
+        userId = objId;
+    }
     const user = await userCollection.findOne({ _id: userId });
     if (!user) throw 'User not found';
     return user;
 };
 
-async function getAllUsers() {
+async function getAllUsers() {//tested
     const userCollection = await users();
     const userList = await userCollection.find({}).toArray();
     return userList;
 };
 
-async function editUser(userId, username, password, nickname) {
-    const user = await this.getUserById(userId);
-		console.log(user);
+async function editUser(userId, username, password, nickname) {//updated string id to objid, tested
+    if(typeof userId =="string")
+    {
+        const objId = ObjectId.createFromHexString(userId);
+        userId = objId;
+    }
+    const userUpdateInfo = {
+        username: username,
+        password: password,
+        nickname: nickname
+    };
 
-		const userUpdateInfo = {
-			username: username,
-            password: password,
-            nickname: nickname
-		};
+    const userCollection = await users();
+    const updateInfo = await userCollection.updateOne({ _id: userId }, { $set: userUpdateInfo });
+    if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
 
-		const userCollection = await users();
-		const updateInfo = await userCollection.updateOne({ _id: userId }, { $set: userUpdateInfo });
-		if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
-
-		return await this.getUserById(id);
+    return await this.getUserById(userId);
 };
 
 async function addPostToUser(userId, postId) { // postId is passed through and stored as a string
--- routes/users.js
+++ routes/users.js
@@ -42,7 +42,7 @@ router.post('/', async (req, res) => {
 	}
 
 	try {
-		const newUser = await userData.addUser(userInfo.username, userinfo.password, userinfo.nickname);
+		const newUser = await userData.createUser(userInfo.username,userInfo.password,userInfo.nickname);//revised userinfo to userInfo, and wrong function name
 		res.json(newUser);
 	} catch (e) {
 		res.sendStatus(500);
@@ -53,10 +53,10 @@ router.patch('/:id', async (req, res) => {
 	const requestBody = req.body;
 	let updatedObject = {};
 	try {
-		const oldUser = await userData.getUserById(req.params.id);
-		if (requestBody.username && requestBody.username !== oldPost.username) updatedObject.username = requestBody.username;
-		if (requestBody.password && requestBody.password !== oldPost.password) updatedObject.password = requestBody.password;
-		if (requestBody.nickname && requestBody.nickname !== oldPost.nickname) updatedObject.nickname = requestBody.nickname;
+		const oldUser = await userData.getUserById(req.params.id);// revised oldPost to oldUser
+		if (requestBody.username && requestBody.username !== oldUser.username) updatedObject.username = requestBody.username; 
+		if (requestBody.password && requestBody.password !== oldUser.password) updatedObject.password = requestBody.password;
+		if (requestBody.nickname && requestBody.nickname !== oldUser.nickname) updatedObject.nickname = requestBody.nickname;
 		if (requestBody.userId && requestBody.userId !== oldUser.userId)
 			updatedObject.userId = requestBody.userId;
 	} catch (e) {
@@ -65,12 +65,21 @@ router.patch('/:id', async (req, res) => {
 	}
 
 	try {
-		const updatedUser = await userData.editUser(req.params.id, updatedObject);
+		const updatedUser = await userData.editUser(req.params.id, updatedObject.username,updatedObject.password,updatedObject.nickname);//updated input parameters
 		res.json(updatedUser);
 	} catch (e) {
 		res.status(500).json({ error: e });
 	}
 });
 
+//create getAll route 
+router.get("/", async (req, res) => {
+    try {
+      const userList = await userData.getAllUsers();
+      res.json(userList);
+    } catch (e) {
+      res.status(500).send();
+    }
+});
 
 module.exports = router;

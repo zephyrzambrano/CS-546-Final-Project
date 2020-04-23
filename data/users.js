63ca8b0ef055bf928b7aa9827253d0ade59b4193
Jugram-Chen <57543961+Jugram-Chen@users.noreply.github.com>
2020-04-22 22:27:46 -0400

    Update users.js

--- data/users.js
+++ data/users.js
@@ -8,8 +8,14 @@ async function createUser(username, password, nickname) { //tested
     if (!password || typeof username !== "string") throw "must provide password";
     if (!nickname || typeof username !== "string") throw "must provide nickname";
     const userCollection = await users();
-    const existUser = await userCollection.findOne({username: username}); 
-    if (existUser) throw `${username} is used, please submit another username`;
+
+    let usernameExist = await userCollection.findOne({ username: username });
+    if (usernameExist !== null)
+        throw "the username is already exist"
+    let nicknameExist = await userCollection.findOne({ nickname: nickname });
+    if (nicknameExist !== null)
+        throw "the nickname is already exist"
+
     let newUser = {
         // userid - object id created by mongodb
         username: username, // string
@@ -20,14 +26,13 @@ async function createUser(username, password, nickname) { //tested
     };
     const newInsertInformation = await userCollection.insertOne(newUser);
     if (newInsertInformation.insertedCount === 0) throw 'Insert failed!';
-    
-	return await this.getUserById(newInsertInformation.insertedId);
+
+    return await this.getUserById(newInsertInformation.insertedId);
 };
 
 async function getUserById(userId) { //updated string id to objid, tested
     const userCollection = await users();
-    if(typeof userId =="string")
-    {
+    if (typeof userId == "string") {
         const objId = ObjectId.createFromHexString(userId);
         userId = objId;
     }
@@ -42,49 +47,95 @@ async function getAllUsers() {//tested
     return userList;
 };
 
-async function editUser(userId, username, password, nickname) {//updated string id to objid, tested
-    if(typeof userId =="string")
-    {
-        const objId = ObjectId.createFromHexString(userId);
-        userId = objId;
-    }
-    const userUpdateInfo = {
-        username: username,
-        password: password,
-        nickname: nickname
+async function editPassword(userId, password) {
+    if (!userId || typeof userId !== "string")
+        throw 'you should input a string as the userId';
+    if (!password || typeof password !== "string")
+        throw 'you should input a string as the password';
+
+    let userObjId = ObjectId.createFromHexString(userId);
+    let userCollection = await users();
+
+    let userUpdateInfo = {
+        password: password
     };
+    let updatedInfo = await userCollection.updateOne({ _id: userObjId }, { $set: userUpdateInfo });
+    if (updatedInfo.modifiedCount === 0) {
+        throw 'could not edit the password successfully';
+    }
+    return this.getUserById(userId);
+};
 
-    const userCollection = await users();
-    const updateInfo = await userCollection.updateOne({ _id: userId }, { $set: userUpdateInfo });
-    if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
+async function editUsername(userId, username) {//the username is unique and modifiable
+    if (!userId || typeof userId !== "string")
+        throw 'you should input a string as the userId';
+    if (!username || typeof username !== "string")
+        throw 'you should input a string as the username';
 
-    return await this.getUserById(userId);
+    let userObjId = ObjectId.createFromHexString(userId);
+    let userCollection = await users();
+    let usernameExist = await userCollection.findOne({ username: username });
+    if (usernameExist)
+        throw "the username is already exisited"
+    else {
+        let userUpdateInfo = {
+            username: username
+        };
+        let updatedInfo = await userCollection.updateOne({ _id: userObjId }, { $set: userUpdateInfo });
+        if (updatedInfo.modifiedCount === 0) {
+            throw 'could not edit the username successfully';
+        }
+        return this.getUserById(userId);
+    }
+};
+
+async function editNickname(userId, nickname) {//the name is unique and modifiable
+    if (!userId || typeof userId !== "string")
+        throw 'you should input a string as the userId';
+    if (!nickname || typeof nickname !== "string")
+        throw 'you should input a string as the nickname';
+
+    let userObjId = ObjectId.createFromHexString(userId);
+    let userCollection = await users();
+    let nicknameExist = await userCollection.findOne({ nickname: nickname });
+    if (nicknameExist)
+        throw "the nickname is already exisited"
+    else {
+        let userUpdateInfo = {
+            nickname: nickname
+        };
+        let updatedInfo = await userCollection.updateOne({ _id: userObjId }, { $set: userUpdateInfo });
+        if (updatedInfo.modifiedCount === 0) {
+            throw 'could not edit the nickname successfully';
+        }
+        return this.getUserById(userId);
+    }
 };
 
 async function addPostToUser(userId, postId) { // postId is passed through and stored as a string
     let currentUser = await this.getUserById(userId);
-    console.log(currentUser);
+    // console.log(currentUser);
 
     const userCollection = await users();
     const updateInfo = await userCollection.updateOne(
-        {_id: ObjectId(userId)},
-        {$addToSet: {posts: postId} }
+        { _id: ObjectId(userId) },
+        { $addToSet: { posts: postId } }
         // {$addToSet: {posts: ObjectId(postId).toString()} }
     );
 
-    if (!updateInfo.matchedCount && ! updateInfo.modifiedCount) throw 'Update failed';
+    if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
 
     return await this.getUserById(userId);
 };
 
 async function removePostFromUser(userId, postId) { // postId is passed through and stored as a string
     let currentUser = await this.getUserById(userId);
-    console.log(currentUser);
+    // console.log(currentUser);
 
     const userCollection = await users();
     const updateInfo = await userCollection.updateOne(
-        {_id: ObjectId(userId)},
-        {$pull: {posts: postId} }
+        { _id: ObjectId(userId) },
+        { $pull: { posts: postId } }
         // {$pull: {posts: ObjectId(postId).toString()} }
     );
     if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
@@ -98,12 +149,12 @@ async function addCommentToUser(userId, commentId) { // commentId is passed thro
 
     const userCollection = await users();
     const updateInfo = await userCollection.updateOne(
-        {_id: ObjectId(userId)},
-        {$addToSet: {comments: commentId} }
+        { _id: ObjectId(userId) },
+        { $addToSet: { comments: commentId } }
         // {$addToSet: {comments: ObjectId(commentId).toString()} }
     );
 
-    if (!updateInfo.matchedCount && ! updateInfo.modifiedCount) throw 'Update failed';
+    if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
 
     return await this.getUserById(userId);
 
@@ -115,8 +166,8 @@ async function removeCommentFromUser(userId, commentId) { // commentId is passed
 
     const userCollection = await users();
     const updateInfo = await userCollection.updateOne(
-        {_id: ObjectId(userId)},
-        {$pull: {comments: commentId} }
+        { _id: ObjectId(userId) },
+        { $pull: { comments: commentId } }
         // {$pull: {comments: ObjectId(commentId).toString()} }
     );
     if (!updateInfo.matchedCount && !updateInfo.modifiedCount) throw 'Update failed';
@@ -129,9 +180,11 @@ module.exports = {
     createUser,
     getUserById,
     getAllUsers,
-    editUser,
+    editUsername,
+    editPassword,
+    editNickname,
     addPostToUser,
-    removePostFromUser,
-    addCommentToUser,
-    removeCommentFromUser
+    removePostFromUser
+    // addCommentToUser,
+    // removeCommentFromUser
 }

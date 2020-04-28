c8125e1f663f13fdbe10b9d2813d80db3119aba8
chuanyao17 <chuanyao17@gmail.com>
2020-04-28 19:10:16 -0400

    add postinstance

---
+++ public/postInstance/Yao_PostInstance.js
@@ -0,0 +1,41 @@
+post1 = {
+    // postID: "",The server automatically generates the data
+	topic: "LIGHTWEIGHT. VERSATILE. CLASSIC. Boots",
+    // userID: "",
+    content: "Made in our Portland, Oregon, factory, the Mountain Pass was created to be a lighter weight and more versatile hiking boot that still reflects our classic Danner styling.",
+	photoArr: ["danner_boots.jpg","danner_boots01.jpg"],
+	commentIdArr: [],
+    tagArr: ["footwear", "casual"],
+    //likeCount: "10",
+    //dislikeCount: "9",
+    //viewCount: "8",
+    //date: The server automatically generates the data
+};
+
+post2 = {
+    // postID: "",The server automatically generates the data
+	topic: "My best Burton for snowboarding",
+    // userID: "",
+    content: "Colorful, useful, satisfied",
+	photoArr: ["burton01.jpg","burton02.jpg"],
+	commentIdArr: [],
+    tagArr: ["sports"],
+    //likeCount: "7",
+    //dislikeCount: "6",
+    //viewCount: "5",
+    //date: The server automatically generates the data
+};
+
+post3 = {
+    // postID: "",
+	topic: "Supreme Takashi Murakami COVID-19 Relief Box Logo Tee White",
+    // userID: "",
+    content: "Supreme has collaborated with Takashi Murakami for an exclusive COVID-19 Relief Box Logo Tee. Supreme will donate 100% of the proceeds to charity in support of youth and families facing homelessness during the COVID-19 crisis.",
+	photoArr: ["supreme01.jpg","supreme02.jpg"],
+	commentIdArr: [],
+    tagArr: ["casual"],
+    //likeCount: "10",
+    //dislikeCount: "99",
+    //viewCount: "800",
+    //date: The server automatically generates the data
+};

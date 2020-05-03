7f79893e217753e2e299be4a4b05b2ebc255671a
Jugram-Chen <57543961+Jugram-Chen@users.noreply.github.com>
2020-05-03 16:19:13 -0400

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -2,161 +2,94 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const postData = data.posts;
+const userData = data.users;
+const commentData = data.comments;
+
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
 
 router.get('/:id', async (req, res) => {
     try {
-        let user = await postData.getPostById(req.params.id);
-        res.json(user);//render the post here
-    } catch (e) {
+        let userLogin=null;
+        if(req.session.userId)
+            userLogin=await userData.getUserById(req.session.userId);
+        await postData.addViewCount(req.params.id);//Each time this address is accessed, viewCount++
+        let postInfo = await postData.getPostById(req.params.id);
+        let commentsInfo = [];
+        for (let i = 0; i < postInfo.commentIdArr.length; i++) {
+            let thisComment = await commentData.getCommentById(postInfo.commentIdArr[i]);
+            let commentCreaterInfo = await userData.getUserById(thisComment.userId);
+            thisComment.userNickname = commentCreaterInfo.nickname;
+            commentsInfo.push(thisComment);
+        }
+        res.json({ postInfo, commentsInfo, userLogin});
+        // res.render('home/home.handlebars',{ postInfo, commentsInfo, userLogin});
+    } catch (error) {
         res.status(404).json({ error: 'Post not found' });
     }
 });
 
-router.get('/tag', async (req, res) => {
+router.post('/like', async (req, res) => {
     try {
-        let tagInReq = req.query["tag"];
-        let postIncludeTag = await postData.getPostByOneTag(tagInReq);
-        res.json(postIncludeTag);//render the post here
-    } catch (e) {
-        res.status(404).json({ error: 'Post not found with that tag' });
+        if (!req.body)
+            throw "need userId and postId";
+        if (!req.body.postId)
+            throw "need postId";
+        if (!req.body.userId)
+            throw "need userId";
+        let updatedPost = await postData.addLikeCount(req.body.postId, req.body.userId);
+        res.send(updatedPost);
+    } catch (error) {
+        res.status(404).send(error);
     }
 });
 
-router.post('/createPost', async (req, res) => {
-    let postInfo = req.body;
-
-    if (!postInfo) {
-        res.status(400).json({ error: 'You must provide data to create a post' });
-        return;
-    }
-
-    if (!postInfo.userId) {
-        res.status(400).json({ error: 'You must provide a userId to create a post' });
-        return;
-    }
-
-    if (!postInfo.content) {
-        res.status(400).json({ error: 'You must provide a content to create a post' });
-        return;
-    }
-
-    if (!postInfo.photoArr) {
-        res.status(400).json({ error: 'You must provide a photo array to create a post' });
-        return;
-    }
-
-    if (!postInfo.tagArr) {
-        res.status(400).json({ error: 'You must provide a tag array to create a post' });
-        return;
-    }
-
+router.post('/dislike', async (req, res) => {
     try {
-        let newPost = await postData.createPost(postInfo.topic, postInfo.userId, postInfo.content, postInfo.photoArr, postInfo.tagArr);
-        res.json(newPost);
-    } catch (e) {
-        res.sendStatus(500);
+        if (!req.body)
+            throw "need userId and postId";
+        if (!req.body.postId)
+            throw "need postId";
+        if (!req.body.userId)
+            throw "need userId";
+        let updatedPost = await postData.addLikeCount(req.body.postId, req.body.userId);
+        res.send(updatedPost);
+    } catch (error) {
+        res.status(404).send(error);
     }
 });
 
-router.patch('/editPost', async (req, res) => {
-    let postInfo = req.body;
-
-    if (!postInfo) {
-        res.status(400).json({ error: 'You must provide data to edit a post' });
-        return;
-    }
-    if (!postInfo.postId) {
-        res.status(400).json({ error: 'You must provide a postId to edit a post' });
-        return;
-    }
-    if (!postInfo.newContent) {
-        res.status(400).json({ error: 'You must provide a content to edit a post' });
-        return;
-    }
-
+router.post('/editConetent', async (req, res) => {
     try {
-        let updatedContent = await postData.editContent(postInfo.postId, postInfo.newContent);
-        res.json(updatedContent);
-    } catch (e) {
-        res.sendStatus(500);
+        if (!req.body)
+            throw "need new content and postId";
+        if (!req.body.postId)
+            throw "need postId";
+        if (!req.body.newContent)
+            throw "need userId";
+        let updatedPost = await postData.editContent(req.body.postId, req.body.newConetent)
+        res.send(updatedPost);
+    } catch (error) {
+        res.status(404).send(error);
     }
 });
 
-router.patch('/addLikeCount', async (req, res) => {
-    let postInfo = req.body;
-
-    if (!postInfo) {
-        res.status(400).json({ error: 'You must provide data to edit a post' });
-        return;
-    }
-    if (!postInfo.postId) {
-        res.status(400).json({ error: 'You must provide a postId to edit a post' });
-        return;
-    }
-    if (!postInfo.userId) {
-        res.status(400).json({ error: 'You must provide a content to edit a post' });
-        return;
-    }
-
+router.post('/addComment', async (req, res) => {
     try {
-        let updatedLikeCount = await postData.addLikeCount(postInfo.postId, postInfo.userId)
-        res.json(updatedLikeCount);
-    } catch (e) {
-        res.sendStatus(500);
+        if (!req.body)
+            throw "need userId postId commentContent to create a comment";
+        if (!req.body.postId)
+            throw "need postId to create a comment";
+        if (!req.body.userId)
+            throw "need userId to create a comment";
+        if (!req.body.commentContent)
+            throw "need commentContent to create a comment";
+        let newComment=await commentData.addComment(req.body.postId,req.body.userId,req.body.commentContent)
+        res.send(newComment);
+    } catch (error) {
+        res.status(404).send(error);
     }
 });
 
-router.patch('/addDislikeCount', async (req, res) => {
-    let postInfo = req.body;
-
-    if (!postInfo) {
-        res.status(400).json({ error: 'You must provide data to edit a post' });
-        return;
-    }
-    if (!postInfo.postId) {
-        res.status(400).json({ error: 'You must provide a postId to edit a post' });
-        return;
-    }
-    if (!postInfo.userId) {
-        res.status(400).json({ error: 'You must provide a content to edit a post' });
-        return;
-    }
-
-    try {
-        let updatedDislikeCount = await postData.addDislikeCount(postInfo.postId, postInfo.userId)
-        res.json(updatedDislikeCount);
-    } catch (e) {
-        res.sendStatus(500);
-    }
-});
-
-router.patch('/viewCount', async (req, res) => {
-    let postInfo = req.body;
-
-    if (!postInfo) {
-        res.status(400).json({ error: 'You must provide data to edit a post' });
-        return;
-    }
-    if (!postInfo.postId) {
-        res.status(400).json({ error: 'You must provide a postId to edit a post' });
-        return;
-    }
-
-    try {
-        let updatedViewCount = await postData.addViewCount(postInfo.postId);
-        res.json(updatedViewCount);
-    } catch (e) {
-        res.sendStatus(500);
-    }
-});
-
-router.delete('/:id', async (req, res) => {
-    try {
-        let deletedPost = await postData.removeCommentIdFromPost(req.params.id);
-        res.json(deletedPost);
-    } catch (e) {
-        res.sendStatus(500);
-    }
-});
 
 module.exports = router;

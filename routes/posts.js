23f3e69c2ca4536bcb5086ece0c3ca8d49f179f5
Jugram-Chen <57543961+Jugram-Chen@users.noreply.github.com>
2020-05-05 19:05:15 -0400

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -4,143 +4,107 @@ const data = require("../data");
 const postData = data.posts;
 const userData = data.users;
 const commentData = data.comments;
-const formidable = require('formidable');
 
+router.use(express.urlencoded({ extended: false }));
+router.use(express.json());
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {//浏览器端发送一个普通的get请求，网址包含postId，返回渲染一个完整的posts网页（包含帖子内容和评论）
     try {
         let userLogin = null;
-        if(req.session){
+        if (req.session) {
             if (req.session.userId)
                 userLogin = await userData.getUserById(req.session.userId);
         }
-
-        // res.send({ postInfo, commentsInfo, userLogin});
-        res.render('posts/posts.handlebars',{
-            postInfo,
-            commentsInfo,
-            userLogin
-        });
+        await postData.addViewCount(req.params.id);//Each time this address is accessed, viewCount++
+        let postInfo = await postData.getPostById(req.params.id);
+        let commentsInfo = [];
+        for (let i = 0; i < postInfo.commentIdArr.length; i++) {
+            let thisComment = await commentData.getCommentById(postInfo.commentIdArr[i]);
+            let commentCreaterInfo = await userData.getUserById(thisComment.userId);
+            thisComment.userNickname = commentCreaterInfo.nickname;
+            commentsInfo.push(thisComment);
+        }
+        // res.json({ postInfo, commentsInfo, userLogin});
+        res.render('posts/posts.handlebars', { postInfo, commentsInfo, userLogin });
     } catch (error) {
         res.status(404).json({ error: 'Post not found' });
     }
 });
 
-router.post('/like', async (req, res) => {
+router.get('/like', async (req, res) => {//浏览器端发送一个Ajax的get请求，包含postId的参数，返回这个post更新后的详细数据（不包含comment）
     try {
+        if (!req.session)
+            throw "you don't have the cookie"
+        if (!req.session.userId)
+            throw "login first,then like it"
+        if (!req.query)
+            throw "need info to like it";
+        if (!req.query.postId)
+            throw "need postId to like it";
+        let updatedPost = await postData.addLikeCount(req.query.postId, req.session.userId);
+        res.send(updatedPost);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+router.get('/dislike', async (req, res) => {//浏览器端发送一个Ajax的get请求，包含postId的参数，返回这个post更新后的详细数据（不包含comment）
+    try {
+        if (!req.session)
+            throw "you don't have the cookie"
+        if (!req.session.userId)
+            throw "login first,then dislike it"
+        if (!req.query)
+            throw "need info to dislike it";
+        if (!req.query.postId)
+            throw "need postId to dislike it";
+        let updatedPost = await postData.addLikeCount(req.query.postId, req.session.userId);
+        res.send(updatedPost);
+    } catch (error) {
+        res.status(404).send(error);
+    }
+});
+
+router.post('/editConetent', async (req, res) => {//（目前还在讨论）浏览器端发送一个普通的post请求，包含postId与新的content，返回更新后的post详细数据
+    try {
+        if (!req.session)
+            throw "you don't have the cookie"
+        if (!req.session.userId)
+            throw "login first,then edit content"
         if (!req.body)
-            throw "need userId and postId";
+            throw "need new content and postId";
         if (!req.body.postId)
             throw "need postId";
-        if (!req.body.userId)
-            throw "need userId";
-        let updatedPost = await postData.addLikeCount(req.body.postId, req.body.userId);
+        if (!req.body.newContent)
+            throw "new Content";
+        let postToEdit = await postData.getPostById(req.body.postId);//这里判断想要修改post content的人是不是真正的写这个post的人
+        if (postToEdit.userId !== req.session.userId)//这里判断想要修改post content的人是不是真正的写这个post的人
+            throw "Your id is not the same as the userId of the post!!"
+        let updatedPost = await postData.editContent(req.body.postId, req.body.newConetent);
         res.send(updatedPost);
-
-        let postArr = await postData.getAllPost();
-        for (let i = 0; i < postArr.length; i++) {
-            let temp = await userData.getUserById(postArr[i].userId);
-            postArr[i].userNickname = temp.nickname;
-        }
-        // res.send({ postArr, userLogin });
-        res.render('home/home.handlebars',{postArr,userLogin});
-
     } catch (error) {
         res.status(404).send(error);
     }
 });
 
-router.post('/multTags', async (req, res) => {
+router.post('/addComment', async (req, res) => {//发送一个Ajax的post请求，包含postId与commmet content，返回新的comment信息
     try {
+        if (!req.session)
+            throw "you don't have the cookie"
+        if (!req.session.userId)
+            throw "login first,then make commnet"
         if (!req.body)
-            throw "need tags";
-        if (!req.body.tagArr || !Array.isArray(req.body.tagArr))
-            throw "need a tag Array";
-        // console.log(req.body.tagArr);
-        let postArr = await postData.getPostByMultTag(req.body.tagArr)
-        res.send(postArr);
+            throw "need info to create a comment";
+        if (!req.body.postId)
+            throw "need postId to create a comment";
+        if (!req.body.commentContent)
+            throw "need commentContent to create a comment";
+        let newComment = await commentData.addComment(req.body.postId, req.session.userId, req.body.commentContent)
+        res.send(newComment);//这代表ok，如果你用的是ajax请求，你需要自己在浏览器端重新刷新网页
     } catch (error) {
         res.status(404).send(error);
     }
 });
-router.get("/search", async (req, res) => {
-    try {
-        if (!req.query)
-            throw "need string to search";
-        if (!req.query.searchString)
-            throw "need string to search!";
-        let postArr = await postData.getPostByString(req.query.searchString);
-        res.send(postArr);
-    } catch (error) {
-        res.status(404).send(error);
-    }
-})
 
-router.post('/createPost', async (req, res) => {//这是靠谱写法
-    const form = new formidable.IncomingForm();//创建formidable解析器
-    form.uploadDir = path.join(__dirname, '../', 'public', 'images');//设置上传的存储路径
-    form.keepExtensions = true;//保留后缀名
-    form.parse(req, async (err, fields, files) => {
-        try {
-            if (!fields)
-                throw "need data to create post";
-            if (!fields.topic)
-                throw "need topic to create post "
-            if (!fields.userId)
-                throw "need userId to create post"
-            if (!fields.content)
-                throw "need content to create post"
-            if (!fields.tagArr || !Array.isArray(fields.tagArr))
-                throw "need a tagArr to create post";
-
-            let photoArr = [];
-            if (files.photo1)
-                photoArr.push("http://localhost:3000/public"+files.photo1.path.split('public')[1]);
-            if (files.photo2)
-                photoArr.push("http://localhost:3000/public"+files.photo2.path.split('public')[1]);
-            if (files.photo3)
-                photoArr.push("http://localhost:3000/public"+files.photo3.path.split('public')[1]);
-
-            let newPost = await postData.createPost(
-                fields.topic,
-                fields.userId,
-                fields.content,
-                photoArr,
-                fields.tagArr
-            )
-            res.send(newPost);
-        } catch (error) {
-            res.status(404).send(error);
-        }
-    })
-});
 
 module.exports = router;
-
-// router.post('/createPost', async (req, res) => {//这里写的有问题，要上传文件的话需要配合前端使用formidable和FormData进行上传
-    //     try {
-    //         if (!req.body)
-    //             throw "need data to create post";
-    //         if (!req.body.topic)
-    //             throw "need topic to create post "
-    //         if (!req.body.userId)
-    //             throw "need userId to create post"
-    //         if (!req.body.content)
-    //             throw "need content to create post"
-    //         if (!req.body.photoArr || !Array.isArray(req.body.photoArr))
-    //             throw "need a photo array to create post";
-    //         if (!req.body.tagArr || !Array.isArray(req.body.tagArr))
-    //             throw "need a tagArr to create post";
-
-    //         let newPost = await postData.createPost(
-    //             req.body.topic,
-    //             req.body.userId,
-    //             req.body.content,
-    //             req.body.photoArr,
-    //             req.body.tagArr
-    //         )
-    //         res.send(newPost);
-    //     } catch (error) {
-    //         res.status(404).send(error);
-    //     }
-    // });

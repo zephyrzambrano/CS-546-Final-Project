5ab6ba49a97a0c8fcbbe3e6fe80b7fdc6b71222b
chuanyao17 <chuanyao17@gmail.com>
2020-05-05 22:45:12 -0400

    implemented report.handlebar

--- routes/reports.js
+++ routes/reports.js
@@ -2,15 +2,60 @@ const express = require("express");
 const router = express.Router();
 const data = require("../data");
 const reportData = data.reports;
+const userData = data.users;
+const postData = data.posts;
 
 module.exports = router;
 
 
 router.get("/form", async (req, res) => {
-  // if (!req.session.user) {		//if not logged in, redirect to the homepage
-	// 	return res.redirect('/homePage');
-	// }
-    res.render('reports/report-form');
+  if (!req.session.userId) {		//if not logged in, redirect to the homepage
+    return res.redirect('/homePage');
+  }
+  
+
+
+  try{
+    const userId = req.session.userId;
+    const userLogin = await userData.getUserById(userId);
+    // const post = await postData.getPostById(postId);
+    const mockTopic = await postData.getPostById("5eb2227cc74479564c3d93cc");//mock
+    res.render('reports/report-form',{userLogin,'reported-post':mockTopic.topic});
+    // res.render('reports/report-form',{userLogin,'reported-post':post.topic});
+  }catch(e){
+    res.status(404).json({ error: e });
+  }
+});
+
+router.post("/form", async (req, res) => {
+  const userId = req.session.userId;
+  const userLogin = await userData.getUserById(userId);
+  const mockTopic = await postData.getPostById("5eb2227cc74479564c3d93cc");//mock
+  //const post = await postData.getPostById(postId);
+    try
+    {
+      if(Object.keys(req.body).length!=0)
+      {
+        let reason = req.body.reason;
+        if(typeof reason == "string")
+        {
+          reason=[reason];
+        }
+        await reportData.addReport(userId,"5eb2227cc74479564c3d93cc",reason); //mock postId
+        // await reportData.addReport(userId,post._id,reason);
+        res.render('reports/report-submitted',{userLogin});
+        return;
+      }
+      else
+      {
+        throw `Error: You must select a reason for reporting a post!`;
+      }
+    }
+    catch(e)
+    {
+      res.render('reports/report-form',{noReason:true, message:e, userLogin,'reported-post':mockTopic.topic});//mock postTopic
+      // res.render('reports/report-form',{noReason:true, message:e, userLogin,'reported-post':post.topic});
+    } 
 });
 
 
@@ -32,25 +77,3 @@ router.get("/:id", async (req, res) => {
     }
 });
 
-router.post("/", async (req, res) => {
-    const postReportData = req.body;
-    if (!postReportData.userId) {
-      res.status(400).json({ error: 'You must provide an user id for report' });
-      return;
-    }
-    if (!postReportData.postId) {
-      res.status(400).json({ error: 'You must provide a post id for report' });
-      return;
-    }
-    if (!postReportData.reason) {
-      res.status(400).json({ error: 'You must provide reason for report' });
-      return;
-    }
-    try {
-      const {userId, postId, reason} = postReportData;
-      const newReport = await reportData.addReport(userId, postId, reason);
-      res.status(200).json(newReport);
-    } catch (e) {
-      res.status(400).json({ error: e });
-    }
-});
